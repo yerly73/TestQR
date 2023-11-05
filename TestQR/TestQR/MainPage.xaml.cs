260072583c5b9394f25db8076fede669fb470c30
yerly73 <aysa.yerly@tescup.edu.pe>
2023-11-04 20:24:54 -0500

    Agregar archivos de proyecto.

---
+++ TestQR/TestQR/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace TestQR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await TestQR.App.Current.MainPage.Navigation.PushModalAsync(scan);
+
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await TestQR.App.Current.MainPage.Navigation.PopModalAsync();
+
+                    if (!string.IsNullOrEmpty(result.Text))
+                        txtBarCode.Text = result.Text;
+
+                });
+            };
+        }
+    }
+}
